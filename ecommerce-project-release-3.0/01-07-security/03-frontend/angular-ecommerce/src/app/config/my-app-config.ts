53d614414e89b0dc53b9860442a792497f975864
Alex Lin <alexlin1822@hotmail.com>
2022-03-11 16:22:29 -0500

    Fixed bug in registration form

--- ecommerce-project-release-3.0/01-07-security/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ ecommerce-project-release-3.0/01-07-security/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc: {
-        clientId: '<<ADD-YOUR-CLIENT-ID>>>',
-        issuer: 'https://<<ADD-YOUR-DEV-DOMAIN>>/oauth2/default',
+        clientId: '0oa44q0g12WlsSyar5d7',
+        issuer: 'https://dev-8576707.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
